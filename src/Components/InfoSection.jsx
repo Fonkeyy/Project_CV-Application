b1838a48f6d1b482cf22efe9baa828d9921c1de0
Fonkeyy <cedrick.guevel@gmail.com>
2023-08-01 12:29:20 +0200

    add InfoSection logic

--- src/Components/InfoSection.jsx
+++ src/Components/InfoSection.jsx
@@ -1,159 +1,66 @@
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-
+import React, { useState } from 'react';
 import InfoComponent from './InfoComponent';
 import Button from './Button';
 
+const initialFields = [
+    { id: 'tel', label: 'Phone Number', type: 'tel', placeholder: 'Enter your phone', value: '' },
+    { id: 'email', label: 'Email', type: 'email', placeholder: 'Enter your email', value: '' },
+    { id: 'github', label: 'Github', type: 'url', placeholder: 'Enter your Github', value: '' },
+    { id: 'website', label: 'Website', type: 'url', placeholder: 'Enter your website', value: '' },
+];
+
 const InfoSection = () => {
     const [isSubmitted, setIsSubmitted] = useState(false);
-
-    const [phoneValue, setPhoneValue] = useState('');
-    const [emailValue, setEmailValue] = useState('');
-    const [githubValue, setGithubValue] = useState('');
-    const [websiteValue, setWebsiteValue] = useState('');
+    const [fields, setFields] = useState(initialFields);
+    const [isOvered, setIsOvered] = useState(false);
 
     const handleOnChange = (event) => {
-        const target = event.target;
-        console.log(target);
-
-        console.log(phoneValue);
-
-        target.id == 'tel' && setPhoneValue(target.value);
-        target.id == 'email' && setEmailValue(target.value);
-        target.id == 'github' && setGithubValue(target.value);
-        target.id == 'website' && setWebsiteValue(target.value);
-
-        // // switch (target.placeholder) {
-        // //     case 'Enter your phone':
-        // //         setPhoneValue(target.value);
-        // //         break;
-        // //     case 'Enter your email':
-        // //         setEmailValue(target.value);
-        // //         break;
-        // //     case 'Enter your Github':
-        // //         setGithubValue(target.value);
-        // //         break;
-        // //     case 'Enter your website':
-        // //         setWebsiteValue(target.value);
-        // //         break;
-        // //     default:
-        // //         break;
-        // // }
+        const { id, value } = event.target;
+        setFields((prevFields) => prevFields.map((field) => (field.id === id ? { ...field, value } : field)));
     };
 
     const handleDelete = (id) => {
-        setInfoComponent((prevComponent) => prevComponent.filter((component) => component.props.id !== id));
+        setFields((prevFields) => prevFields.filter((field) => field.id !== id));
     };
 
-    const [infoComponent, setInfoComponent] = useState([
-        <InfoComponent
-            key={uuidv4()}
-            label="Phone Number"
-            id="tel"
-            type="tel"
-            placeholder="Enter your phone"
-            value={phoneValue}
-            onChange={handleOnChange}
-            onDelete={handleDelete}
-        />,
-        <InfoComponent
-            key={uuidv4()}
-            label="Email"
-            id="email"
-            type="email"
-            placeholder="Enter your email"
-            value={emailValue}
-            onChange={handleOnChange}
-            onDelete={handleDelete}
-        />,
-        <InfoComponent
-            key={uuidv4()}
-            label="Github"
-            id="github"
-            type="url"
-            placeholder="Enter your Github"
-            value={githubValue}
-            onChange={handleOnChange}
-            onDelete={handleDelete}
-        />,
-        <InfoComponent
-            key={uuidv4()}
-            label="Website"
-            id="website"
-            type="url"
-            placeholder="Enter your website"
-            value={websiteValue}
-            onChange={handleOnChange}
-            onDelete={handleDelete}
-        />,
-    ]);
-
-    useEffect(() => {
-        // Update the infoComponent array whenever any of the state variables change
-        setInfoComponent([
-            <InfoComponent
-                key={uuidv4()}
-                label="Phone Number"
-                id="tel"
-                type="tel"
-                placeholder="Enter your phone"
-                value={phoneValue}
-                onChange={handleOnChange}
-                onDelete={handleDelete}
-            />,
-            <InfoComponent
-                key={uuidv4()}
-                label="Email"
-                id="email"
-                type="email"
-                placeholder="Enter your email"
-                value={emailValue}
-                onChange={handleOnChange}
-                onDelete={handleDelete}
-            />,
-            <InfoComponent
-                key={uuidv4()}
-                label="Github"
-                id="github"
-                type="url"
-                placeholder="Enter your Github"
-                value={githubValue}
-                onChange={handleOnChange}
-                onDelete={handleDelete}
-            />,
-            <InfoComponent
-                key={uuidv4()}
-                label="Website"
-                id="website"
-                type="url"
-                placeholder="Enter your website"
-                value={websiteValue}
-                onChange={handleOnChange}
-                onDelete={handleDelete}
-            />,
-        ]);
-    }, [phoneValue, emailValue, githubValue, websiteValue]);
+    const handleSubmit = () => {
+        setIsSubmitted(true);
+    };
 
     return (
         <>
             {isSubmitted ? (
-                <></>
+                <div
+                    onMouseEnter={() => {
+                        setIsOvered(true);
+                    }}
+                    onMouseLeave={() => {
+                        setIsOvered(false);
+                    }}>
+                    {fields.map((field) => (
+                        <div key={field.id}>
+                            <p>{field.label}</p>
+                            <p>{field.value}</p>
+                        </div>
+                    ))}
+                    {isOvered && <Button className="edit-btn" onClick={() => setIsSubmitted(false)} />}
+                </div>
             ) : (
                 <>
-                    {infoComponent.map((item) => {
-                        return (
-                            <div key={item.props.id} className="">
-                                {item}
-                            </div>
-                        );
-                    })}
-                    <Button
-                        className="submit-btn"
-                        onClick={() => {
-                            setIsSubmitted(true);
-                        }}
-                    />
+                    {fields.map((field) => (
+                        <div key={field.id}>
+                            <InfoComponent
+                                label={field.label}
+                                id={field.id}
+                                type={field.type}
+                                placeholder={field.placeholder}
+                                value={field.value}
+                                onChange={handleOnChange}
+                                onDelete={handleDelete}
+                            />
+                        </div>
+                    ))}
+                    <Button className="submit-btn" onClick={handleSubmit} />
                 </>
             )}
         </>
