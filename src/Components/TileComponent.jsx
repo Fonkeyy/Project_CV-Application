0ab56ab6d0f72cf93d352d571d66d656f7916575
Fonkeyy <cedrick.guevel@gmail.com>
2023-08-03 14:36:14 +0200

    add input date to TileComponent

--- src/Components/TileComponent.jsx
+++ src/Components/TileComponent.jsx
@@ -6,10 +6,20 @@ import InputField from './InputField';
 import Button from './Button';
 import TileBtnWrapper from './TileBtnWrapper';
 
+const currentDate = new Date();
+// *Get the year and month parts
+const year = currentDate.getFullYear();
+const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+
+// *Format the value to set in the input field
+const todayDate = `${year}-${month}`;
+
 const TileComponent = ({ onSubmit, onDelete, id, field1Name, field2Name }) => {
     const [isSubmitted, setIsSubmitted] = useState(false);
     const [value1, setValue1] = useState('');
     const [value2, setValue2] = useState('');
+    const [value3, setValue3] = useState(todayDate);
+    const [value4, setValue4] = useState(todayDate);
 
     const [isMouseOver, setIsMouseOver] = useState(false);
 
@@ -30,6 +40,8 @@ const TileComponent = ({ onSubmit, onDelete, id, field1Name, field2Name }) => {
 
         target.id === 'input1' && setValue1(target.value);
         target.id === 'input2' && setValue2(target.value);
+        target.id === 'input3' && setValue3(target.value);
+        target.id === 'input4' && setValue4(target.value);
     };
 
     // * On edit set isSubmitted to false and pass the info to parent component
@@ -50,6 +62,8 @@ const TileComponent = ({ onSubmit, onDelete, id, field1Name, field2Name }) => {
                 <>
                     <p className="value1-submitted">{value1}</p>
                     <p className="value2-submitted">{value2}</p>
+                    <p className="date-submitted">{value3}</p>
+                    <p className="date-submitted">{value4}</p>
 
                     {isMouseOver && TileBtnWrapper({ handleEdit, onDelete, id })}
                 </>
@@ -71,6 +85,20 @@ const TileComponent = ({ onSubmit, onDelete, id, field1Name, field2Name }) => {
                         value={value2}
                         onChange={handleChange}
                     />
+                    <input
+                        id="input3"
+                        className="input-date"
+                        type="month"
+                        placeholder="Start date"
+                        value={value3}
+                        onChange={handleChange}></input>
+                    <input
+                        id="input4"
+                        className="input-date"
+                        type="month"
+                        placeholder="End date"
+                        value={value4}
+                        onChange={handleChange}></input>
                     <Button className="submit-btn-30" onClick={handleSubmit} />
                 </>
             )}
