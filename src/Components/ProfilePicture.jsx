efde8b87b2a9bee91d4b1cef42d269853c78e051
Fonkeyy <cedrick.guevel@gmail.com>
2023-08-03 09:28:42 +0200

    add file explorer + comment ProfilePicture

--- src/Components/ProfilePicture.jsx
+++ src/Components/ProfilePicture.jsx
@@ -2,45 +2,39 @@
 import React, { useState } from 'react';
 
 import Button from './Button';
-import InputField from './InputField';
 
 import '../CSS/Components/ProfilePicture.css';
-import '../CSS/Components/InputField.css';
 
 const ProfilePicture = () => {
-    const [url, setUrl] = useState(
-        'https://imgv3.fotor.com/images/slider-image/A-blurry-image-of-a-woman-wearing-red.jpg'
-    );
+    const [url, setUrl] = useState('');
     const [isMouseOver, setIsMouseOver] = useState(false);
     const [isEditClick, setIsEditClick] = useState(false);
 
-    const handleChange = (event) => {
-        setUrl(event.target.value);
+    const handleEdit = () => {
+        setIsEditClick(true);
+        setIsMouseOver(false);
     };
 
-    const handleSubmit = () => {
+    // * Open file explorer and set the chosen file as url state
+    // * On file change get the file, create object URL with the file and setUrl with this object
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        const imageUrl = URL.createObjectURL(file);
+        setUrl(imageUrl);
         setIsEditClick(false);
+        setIsMouseOver(false);
     };
 
+    // * Add url to profile-picture style, manage button appearance depending on file already added or not
     return (
         <div
             id="profile-picture-container"
             onMouseEnter={() => setIsMouseOver(true)}
             onMouseLeave={() => setIsMouseOver(false)}>
             <div id="profile-picture" style={{ backgroundImage: `url(${url})` }}></div>
-            {isMouseOver && <Button className="edit-btn" onClick={() => setIsEditClick(true)} />}
-            {isEditClick && (
-                <>
-                    <InputField
-                        type="url"
-                        className="input-text"
-                        placeholder="Enter the URl"
-                        value={url}
-                        onChange={handleChange}
-                    />
-                    <Button className="submit-btn" onClick={handleSubmit} />
-                </>
-            )}
+            {isMouseOver && url && <Button className="edit-btn add-profile-picture" onClick={handleEdit} />}
+            {isMouseOver && !url && <Button className="add-btn add-profile-picture" onClick={handleEdit} />}
+            {isEditClick && <input type="file" onChange={handleFileChange} />}
         </div>
     );
 };
