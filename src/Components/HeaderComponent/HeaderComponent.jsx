c46c0bdda0c8643d4d0ae7d8217002e804710d24
Fonkeyy <cedrick.guevel@gmail.com>
2023-10-11 13:14:10 +0200

    change icons color

--- src/Components/HeaderComponent/HeaderComponent.jsx
+++ src/Components/HeaderComponent/HeaderComponent.jsx
@@ -61,7 +61,7 @@ const HeaderComponent = ({ id, inputClassName, placeholder }) => {
                         value={inputValue}
                         onChange={handleInputChange}></input>
 
-                    {!isValidated && <Button className="submit_btn " onClick={handleSubmit} />}
+                    {!isValidated && <Button className="submit_btn submit_white " onClick={handleSubmit} />}
                 </>
             )}
         </div>
