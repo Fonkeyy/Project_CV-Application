9388a1fe2e9a6e85bf81bf1d5ac5da57b415a0a6
Fonkeyy <cedrick.guevel@gmail.com>
2023-10-14 08:26:28 +0200

    new path for header-bg

--- src/Components/Sections/HeaderSection/HeaderSection.jsx
+++ src/Components/Sections/HeaderSection/HeaderSection.jsx
@@ -7,7 +7,7 @@ import Button from '../../Button/Button';
 import { ValidContext } from '../../../contexts/validContext';
 
 const HeaderSection = () => {
-    const [url, setUrl] = useState('dist/assets/img/home.jpg');
+    const [url, setUrl] = useState('/assets/img/home.jpg');
     // const [url, setUrl] = useState('src/assets/img/laptop.avif');
     const [isMouseOver, setIsMouseOver] = useState(false);
     const [isClicked, setIsClicked] = useState(false);
